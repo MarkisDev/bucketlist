d380735715d632fac64fe6061b42c4d6e8d0b9d0
Rijuth Menon <rijuthm@gmail.com>
2022-05-22 20:32:08 +0530

    added overscroll, added expanded to make the scroll fit the whole column

--- lib/home/view/home.dart
+++ lib/home/view/home.dart
@@ -18,141 +18,145 @@ class HomePage extends StatelessWidget {
           height: height * 0.02,
         ),
         Expanded(
-          child: CustomListView(
-            header: Column(children: [
-              Center(
-                  child: Container(
-                decoration: BoxDecoration(
-                    color: Constants.kprimaryColor,
-                    borderRadius: BorderRadius.all(Radius.circular(18))),
-                width: width * 0.91,
-                height: height * 0.15,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Rijuth',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 26,
-                                letterSpacing: -0.25,
-                              ),
-                            ),
-                            Text(
-                              'Menon',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 26,
-                                letterSpacing: -0.25,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Flexible(
-                        flex: 2,
-                        child: Container(
-                            margin: EdgeInsets.only(left: 41),
-                            padding: EdgeInsets.fromLTRB(7, 7, 7, 7),
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(color: Colors.black, width: 4),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(12))),
-                            child: Text(
-                              'UNODICK',
-                              style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w700,
-                                  letterSpacing: -0.25),
-                            )),
-                      )
-                    ]),
-              )),
-              SizedBox(
-                height: height * 0.02,
-              ),
-            ]),
-            separatorBuilder: (context, _) {
-              return SizedBox(height: height * 0.02);
-            },
-            itemCount: 10,
-            shrinkWrap: true,
-            itemBuilder: (context, _, item) {
-              return Center(
-                child: Container(
-                  width: width * 0.91,
-                  height: height * 0.12,
+          child: GlowingOverscrollIndicator(
+            color: Constants.kprimaryColor,
+            axisDirection: AxisDirection.down,
+            child: CustomListView(
+              header: Column(children: [
+                Center(
+                    child: Container(
                   decoration: BoxDecoration(
-                      color: Constants.ksecondaryBackgroundColor,
+                      color: Constants.kprimaryColor,
                       borderRadius: BorderRadius.all(Radius.circular(18))),
+                  width: width * 0.91,
+                  height: height * 0.15,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Flexible(
-                        flex: 2,
-                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Flexible(
+                          flex: 1,
+                          child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Padding(
-                                padding: const EdgeInsets.only(top: 12.0),
-                                child: Text(
-                                  'Rijuth Menon',
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      color: Colors.white,
-                                      fontFamily: 'Raleway',
-                                      fontWeight: FontWeight.w600,
-                                      letterSpacing: -0.25),
+                              Text(
+                                'Rijuth',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 26,
+                                  letterSpacing: -0.25,
                                 ),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 10.0),
-                                child: Text(
-                                  'NOUDICK',
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.white,
-                                      fontFamily: 'Raleway',
-                                      fontWeight: FontWeight.w600,
-                                      letterSpacing: 0.15),
+                              Text(
+                                'Menon',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 26,
+                                  letterSpacing: -0.25,
                                 ),
-                              ),
-                            ]),
-                      ),
-                      Flexible(
-                        flex: 3,
-                        child: Transform.rotate(
-                          angle: 30 * math.pi / 180,
-                          child: VerticalDivider(
-                            color: Constants.kprimaryColor,
-                            thickness: 5,
+                              )
+                            ],
                           ),
                         ),
-                      ),
-                      Expanded(
-                        child: Text(
-                          '89',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w700),
-                        ),
-                      ),
-                    ],
-                  ),
+                        Flexible(
+                          flex: 2,
+                          child: Container(
+                              margin: EdgeInsets.only(left: 41),
+                              padding: EdgeInsets.fromLTRB(7, 7, 7, 7),
+                              decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.black, width: 4),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(12))),
+                              child: Text(
+                                'UNODICK',
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w700,
+                                    letterSpacing: -0.25),
+                              )),
+                        )
+                      ]),
+                )),
+                SizedBox(
+                  height: height * 0.02,
                 ),
-              );
-            },
+              ]),
+              separatorBuilder: (context, _) {
+                return SizedBox(height: height * 0.02);
+              },
+              itemCount: 10,
+              footer: SizedBox(height: height * 0.02),
+              itemBuilder: (context, _, item) {
+                return Center(
+                  child: Container(
+                    width: width * 0.91,
+                    height: height * 0.12,
+                    decoration: BoxDecoration(
+                        color: Constants.ksecondaryBackgroundColor,
+                        borderRadius: BorderRadius.all(Radius.circular(18))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Flexible(
+                          flex: 2,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 12.0),
+                                  child: Text(
+                                    'Rijuth Menon',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                        fontFamily: 'Raleway',
+                                        fontWeight: FontWeight.w600,
+                                        letterSpacing: -0.25),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10.0),
+                                  child: Text(
+                                    'NOUDICK',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                        fontFamily: 'Raleway',
+                                        fontWeight: FontWeight.w600,
+                                        letterSpacing: 0.15),
+                                  ),
+                                ),
+                              ]),
+                        ),
+                        Flexible(
+                          flex: 3,
+                          child: Transform.rotate(
+                            angle: 30 * math.pi / 180,
+                            child: VerticalDivider(
+                              color: Constants.kprimaryColor,
+                              thickness: 5,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Text(
+                            '89',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
         ),
       ]),
