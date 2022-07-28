b4c2c8ad52176c1d92293c1b6508067af094fbb2
Rijuth Menon <rijuthm@gmail.com>
2022-07-29 03:10:11 +0530

    added getx pattern

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
-import 'package:bucketlist/home/view/loginpage.dart';
 import 'package:flutter/material.dart';
+
 import 'package:get/get.dart';
-import 'package:bucketlist/home/view/home.dart';
-import 'package:bucketlist/utils/constants.dart';
+
+import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Constants.kprimaryBackgroundColor,
-        fontFamily: 'Poppins',
-        // is not restarted.
-        primaryColor: Constants.kprimaryColor,
-      ),
-      home: Loginpage(),
-    );
-  }
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
