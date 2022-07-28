e1762bd79e66fa0cbd0d878eb120c4969fc268ea
Rijuth Menon <rijuthm@gmail.com>
2022-07-29 03:11:44 +0530

    added color constants

---
+++ lib/app/ui/theme/color_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color kprimaryColor = Color(0xffAFFC41);
+Color ksecondaryColor = Color(0xffB8E9C4);
+Color kprimaryBackgroundColor = Colors.black;
+Color ksecondaryBackgroundColor = Color(0xff1d1d1d);
