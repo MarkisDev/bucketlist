dca6e418fa8362be3b7bcf7f37c073e40c70793b
Rijuth Menon <rijuthm@gmail.com>
2022-07-30 07:59:42 +0530

    added function to recurively generate unique id

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,9 +1,13 @@
 import 'package:bucketlist/app/data/models/user_model.dart';
 import 'package:bucketlist/app/data/providers/realtime_provider.dart';
+import 'package:bucketlist/app/data/repositories/user_repository.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:nanoid/async.dart';
 
 class LoginController extends GetxController {
+  final UserRepository repository;
+  LoginController({required this.repository});
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   late UserModel userModel;
   @override
@@ -18,6 +22,17 @@ class LoginController extends GetxController {
 
   @override
   void onClose() {}
+
+  // Generates a 7-letter unique key
+  genUniqueId() async {
+    String bucketId = await nanoid(7);
+    bool res = await RealtimeDb.checkUnique(bucketId);
+    if (res) {
+      await genUniqueId();
+    } else {
+      return bucketId;
+    }
+  }
 
   // Logins user using GoogleSignIn
   loginWithGoogle() async {
@@ -38,15 +53,19 @@ class LoginController extends GetxController {
       }
       String firstName = parts[0];
 
-      userModel = UserModel(
-          fullName: fullName,
-          firstName: firstName,
-          lastName: lastName,
-          email: googleSignInAccount.email,
-          id: googleSignInAccount.id,
-          photoUrl: googleSignInAccount.photoUrl.toString());
+      String bucketId;
 
-      var x = await RealtimeDb.addUser(userModel, googleSignInAccount.id);
+      if (await repository.checkUser(googleSignInAccount.id)) {
+        userModel = UserModel(
+            fullName: fullName,
+            firstName: firstName,
+            lastName: lastName,
+            email: googleSignInAccount.email,
+            id: googleSignInAccount.id,
+            photoUrl: googleSignInAccount.photoUrl.toString(),
+            bucketId: 'Ayy');
+      }
+      var x = await repository.addUser(userModel);
       await Get.offNamed('/home', arguments: userModel);
       return;
     } catch (e) {
