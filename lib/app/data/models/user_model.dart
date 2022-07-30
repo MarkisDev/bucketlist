279820afe651be74a65d15a3c14e20e2f3e51ac0
Rijuth Menon <rijuthm@gmail.com>
2022-07-30 05:46:53 +0530

    added user model

---
+++ lib/app/data/models/user_model.dart
@@ -0,0 +1,16 @@
+class UserModel {
+  late String fullName;
+  late String firstName;
+  late String lastName;
+  late String email;
+  late String id;
+  late String photoUrl;
+
+  UserModel(
+      {required this.fullName,
+      required this.firstName,
+      required this.lastName,
+      required this.email,
+      required this.id,
+      required this.photoUrl});
+}
