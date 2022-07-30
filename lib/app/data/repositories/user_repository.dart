3a3ac592571450d469fe3698f46e9118faae476d
Rijuth Menon <rijuthm@gmail.com>
2022-07-30 07:59:04 +0530

    added user repository

---
+++ lib/app/data/repositories/user_repository.dart
@@ -0,0 +1,16 @@
+import 'package:bucketlist/app/data/models/user_model.dart';
+import 'package:bucketlist/app/data/providers/realtime_provider.dart';
+
+class UserRepository {
+  addUser(UserModel userModel, String bucketId) async {
+    await RealtimeDb.addUser(userModel);
+  }
+
+  checkUser(String uid) async {
+    return await RealtimeDb.checkUser(uid);
+  }
+
+  checkUnique(String bucketId) async {
+    return await RealtimeDb.checkUnique(bucketId);
+  }
+}
