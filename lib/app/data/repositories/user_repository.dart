f0e28f1bc24b5f8035ef166448eef37ebad61100
Rijuth Menon <rijuthm@gmail.com>
2022-07-30 09:05:17 +0530

    merged bucketId into usermodel

--- lib/app/data/repositories/user_repository.dart
+++ lib/app/data/repositories/user_repository.dart
@@ -2,7 +2,7 @@ import 'package:bucketlist/app/data/models/user_model.dart';
 import 'package:bucketlist/app/data/providers/realtime_provider.dart';
 
 class UserRepository {
-  addUser(UserModel userModel, String bucketId) async {
+  addUser(UserModel userModel) async {
     await RealtimeDb.addUser(userModel);
   }
 
