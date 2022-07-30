44762550f34c85d7547a095f2cb14609f9ebb721
Rijuth Menon <rijuthm@gmail.com>
2022-07-30 20:58:06 +0530

    updating buckets in user

--- lib/app/data/providers/realtime_provider.dart
+++ lib/app/data/providers/realtime_provider.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:bucketlist/app/data/models/user_model.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_list.dart';
 
 class RealtimeDb {
   static final FirebaseDatabase _database = FirebaseDatabase.instance;
@@ -17,7 +20,7 @@ class RealtimeDb {
   static addUser(UserModel userModel) async {
     await _database
         .ref('buckets/${userModel.bucketId}')
-        .set({userModel.bucketId: true});
+        .set({"creatorId": userModel.id});
     await _database.ref("users/${userModel.id}").set({
       "fullName": userModel.fullName,
       "firstName": userModel.firstName,
@@ -26,6 +29,29 @@ class RealtimeDb {
       "photoUrl": userModel.photoUrl,
       "bucketId": userModel.bucketId,
       "email": userModel.email,
+    });
+  }
+
+  static registerBucket(UserModel userModel, String bucketId) async {
+    var bucketsRef = await _database.ref("users/${userModel.id}");
+    bucketsRef.runTransaction((value) {
+      Map<String, dynamic> _buckets = Map<String, dynamic>.from(value as Map);
+      if (_buckets.containsKey('buckets')) {
+        List<String> newBuckets =
+            List<String>.from(_buckets['buckets'] as List);
+        // Aborting transaction if user already part of bucket
+        if (newBuckets.contains(bucketId)) {
+          return Transaction.abort();
+        } else {
+          newBuckets.add(bucketId);
+          _buckets['buckets'] = newBuckets;
+        }
+      } else {
+        _buckets['buckets'] = [bucketId];
+      }
+
+      // Return the new data.
+      return Transaction.success(_buckets);
     });
   }
 }
