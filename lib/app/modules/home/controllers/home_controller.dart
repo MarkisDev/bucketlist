69b898d025f01adb1f48d1fbcf9e16b04c09ccf9
Rijuth Menon <rijuthm@gmail.com>
2022-07-31 10:35:54 +0530

    listening to stream to get first stream outpur then bound that to my list.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,4 +1,6 @@
+import 'package:bucketlist/app/data/models/bucket_model.dart';
 import 'package:bucketlist/app/data/repositories/user_repository.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,11 +9,21 @@ class HomeController extends GetxController {
   var args = Get.arguments;
   final UserRepository repository;
   HomeController({required this.repository});
-
+  final bucketList = [].obs;
+  late var subscription;
   final bucketIdController = TextEditingController();
   @override
   void onInit() {
     super.onInit();
+
+    // repository.userBucketStream(args.id).listen((z) {
+    //   // print(z);
+    //   userBucketList.assignAll(z);
+    // });
+
+    subscription = repository.bucketStream(args.id).listen((var x) {
+      bucketList.bindStream(x);
+    });
   }
 
   @override
@@ -22,5 +34,6 @@ class HomeController extends GetxController {
   @override
   void onClose() {
     bucketIdController.dispose();
+    subscription.cancel();
   }
 }
