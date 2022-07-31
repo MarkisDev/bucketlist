49240a42b31af8e2e09c344be480a7e9387182a7
Rijuth Menon <rijuthm@gmail.com>
2022-07-31 10:35:23 +0530

    Made a bucketStream - Returns a Stream of BUCKETS which returns a Stream of BucketInfo (mapped to a bucketModel, added to a list defined outside both map)

--- lib/app/data/repositories/user_repository.dart
+++ lib/app/data/repositories/user_repository.dart
@@ -1,5 +1,7 @@
+import 'package:bucketlist/app/data/models/bucket_model.dart';
 import 'package:bucketlist/app/data/models/user_model.dart';
 import 'package:bucketlist/app/data/providers/realtime_provider.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class UserRepository {
   addUser(UserModel userModel) async {
@@ -16,5 +18,25 @@ class UserRepository {
 
   registerBucket(UserModel userModel, String bucketId) async {
     return await RealtimeDb.registerBucket(userModel, bucketId);
+  }
+
+  bucketStream(String uid) {
+    var bucketSnapshots = RealtimeDb.getBuckets(uid);
+    List<BucketModel> buckets = [];
+    // Returning a STREAM of buckets to be listened to and bounded to the list in controller
+    return bucketSnapshots.map((DatabaseEvent event) {
+      List bucketEntries =
+          List.from(Map.from(event.snapshot.value as Map).keys);
+      for (var bucketId in bucketEntries) {
+        var x = RealtimeDb.getBucketInfo(bucketId);
+        return x.map((DatabaseEvent test) {
+          var userSnapshot = Map.of(test.snapshot.value as Map);
+          BucketModel bucketModel =
+              BucketModel.fromdataSnapshot(dataSnapshot: userSnapshot);
+          buckets.add(bucketModel);
+          return buckets;
+        });
+      }
+    });
   }
 }
