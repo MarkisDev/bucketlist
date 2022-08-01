a7fcc66db2cbc9510cae68ec145541900e374d9a
Rijuth Menon <rijuthm@gmail.com>
2022-08-01 07:48:11 +0530

    broke code into two parts for two different streams.

--- lib/app/data/repositories/user_repository.dart
+++ lib/app/data/repositories/user_repository.dart
@@ -2,6 +2,7 @@ import 'package:bucketlist/app/data/models/bucket_model.dart';
 import 'package:bucketlist/app/data/models/user_model.dart';
 import 'package:bucketlist/app/data/providers/realtime_provider.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:get/get.dart';
 
 class UserRepository {
   addUser(UserModel userModel) async {
@@ -20,23 +21,39 @@ class UserRepository {
     return await RealtimeDb.registerBucket(userModel, bucketId);
   }
 
-  bucketStream(String uid) {
+  userBucketStream(String uid) {
     var bucketSnapshots = RealtimeDb.getBuckets(uid);
-    List<BucketModel> buckets = [];
+
     // Returning a STREAM of buckets to be listened to and bounded to the list in controller
     return bucketSnapshots.map((DatabaseEvent event) {
       List bucketEntries =
           List.from(Map.from(event.snapshot.value as Map).keys);
-      for (var bucketId in bucketEntries) {
-        var x = RealtimeDb.getBucketInfo(bucketId);
-        return x.map((DatabaseEvent test) {
-          var userSnapshot = Map.of(test.snapshot.value as Map);
-          BucketModel bucketModel =
-              BucketModel.fromdataSnapshot(dataSnapshot: userSnapshot);
-          buckets.add(bucketModel);
-          return buckets;
-        });
-      }
+      // for (int i = 0; i < bucketEntries.length; i++) {
+      //   var x = RealtimeDb.getBucketInfo(bucketEntries[i]);
+
+      //   var t = x.map((DatabaseEvent test) {
+      //     var userSnapshot = Map.of(test.snapshot.value as Map);
+      //     BucketModel bucketModel =
+      //         BucketModel.fromdataSnapshot(dataSnapshot: userSnapshot);
+      //     return bucketModel;
+      //   }).listen((event) {
+      //     buckets.add(event);
+      //   });
+      // }
+      return bucketEntries;
+    });
+  }
+
+  bucketsStream(String userBucket) {
+    var x = RealtimeDb.getBucketInfo(userBucket);
+
+    return x.map((DatabaseEvent test) {
+      List<BucketModel> temp = [];
+      var userSnapshot = Map.of(test.snapshot.value as Map);
+      BucketModel bucketModel =
+          BucketModel.fromdataSnapshot(dataSnapshot: userSnapshot);
+      temp.add(bucketModel);
+      return temp;
     });
   }
 }
