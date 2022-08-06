c4230910e7fb7553799777f3a44e6a54439b2b7a
Rijuth Menon <rijuthm@gmail.com>
2022-08-06 17:32:03 +0530

    added bucketInfo controller

---
+++ lib/app/modules/bucketInfo/controllers/bucket_info_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+
+class BucketInfoController extends GetxController {
+  var args = Get.arguments;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+}
