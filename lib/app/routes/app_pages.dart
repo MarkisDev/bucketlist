2cb50957d677c8b1a520ff92bc43b6552b197a17
Rijuth Menon <rijuthm@gmail.com>
2022-08-06 17:33:01 +0530

    added routes for bucketInfo

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 
+import '../modules/bucketInfo/bindings/bucket_info_binding.dart';
+import '../modules/bucketInfo/views/bucket_info_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
@@ -22,6 +24,11 @@ class AppPages {
       name: _Paths.LOGIN,
       page: () => LoginView(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.BUCKET_INFO,
+      page: () => BucketInfoView(),
+      binding: BucketInfoBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,10 +5,12 @@ abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
   static const LOGIN = _Paths.LOGIN;
+  static const BUCKET_INFO = _Paths.BUCKET_INFO;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
   static const LOGIN = '/login';
+  static const BUCKET_INFO = '/bucket-info';
 }
