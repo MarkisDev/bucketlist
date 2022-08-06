1a96734e255d69740d39537973e3faeedbb1b0b8
Rijuth Menon <rijuthm@gmail.com>
2022-08-06 16:59:51 +0530

    added new private bucket

--- lib/app/data/models/bucket_model.dart
+++ lib/app/data/models/bucket_model.dart
@@ -1,14 +1,19 @@
+import 'package:bucketlist/app/data/providers/realtime_provider.dart';
+import 'package:nanoid/async.dart';
+
 class BucketModel {
   late Map creatorInfo;
   late Map members;
   String? bucketId;
   late int totalEntries;
+  late bool private;
 
   BucketModel(
       {required this.creatorInfo,
       required this.members,
       required this.bucketId,
-      required this.totalEntries});
+      required this.totalEntries,
+      required this.private});
 
   /// Constructor to init variables from Firebase
   BucketModel.fromdataSnapshot({required Map dataSnapshot}) {
@@ -16,5 +21,17 @@ class BucketModel {
     members = dataSnapshot['members'];
     bucketId = dataSnapshot['bucketId'];
     totalEntries = dataSnapshot['totalEntries'];
+    private = dataSnapshot['private'];
+  }
+
+  static genId() async {
+    String bucketId = await customAlphabet(
+        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 5);
+    bool res = await RealtimeDb.checkBucket(bucketId);
+    if (res) {
+      await genId();
+    } else {
+      return bucketId;
+    }
   }
 }
