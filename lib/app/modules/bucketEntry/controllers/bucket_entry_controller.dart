c24e70b2cd69d3b839d15d28eb836716a627f203
Rijuth Menon <rijuthm@gmail.com>
2022-08-07 12:34:38 +0530

    added textController for new entries

--- lib/app/modules/bucketEntry/controllers/bucket_entry_controller.dart
+++ lib/app/modules/bucketEntry/controllers/bucket_entry_controller.dart
@@ -1,18 +1,33 @@
 import 'dart:convert';
 
 import 'package:bucketlist/app/data/providers/realtime_provider.dart';
+import 'package:bucketlist/app/modules/bucketInfo/controllers/bucket_info_controller.dart';
+import 'package:bucketlist/app/ui/theme/color_theme.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:get/get.dart';
 
 class BucketEntryController extends GetxController {
-  QuillController quillController = QuillController.basic();
-  final titleName = "".obs;
+  late QuillController quillController;
+  final titleController = TextEditingController();
   final count = 0.obs;
   final bucketModel = Get.arguments['bucketModel'];
   final userModel = Get.arguments['userModel'];
+  late final bucketData;
   @override
   void onInit() {
     super.onInit();
+    var bucketController = Get.find<BucketInfoController>();
+    if (!bucketController.newEntry) {
+      bucketData = Get.arguments['bucketData'];
+      var document = jsonDecode(bucketData['json']);
+      titleController.text = bucketData['title'];
+      quillController = QuillController(
+          document: Document.fromJson(document),
+          selection: TextSelection.collapsed(offset: 0));
+    } else {
+      quillController = QuillController.basic();
+    }
   }
 
   @override
@@ -22,9 +37,11 @@ class BucketEntryController extends GetxController {
 
   @override
   void onClose() {
+    var bucketController = Get.find<BucketInfoController>();
+
     var json = jsonEncode(quillController.document.toDelta().toJson());
     var data = {
-      "title": titleName.value,
+      "title": titleController.text,
       "json": json,
       "creatorInfo": {
         "id": userModel.id,
@@ -34,6 +51,37 @@ class BucketEntryController extends GetxController {
         "email": userModel.email,
       }
     };
-    RealtimeDb.addBucketEntry(bucketModel.bucketId, data);
+    if (bucketController.newEntry) {
+      RealtimeDb.addBucketEntry(bucketModel.bucketId, data, userModel.id);
+      Get.snackbar(
+        'Success!',
+        'Added new entry!',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: kprimaryColor,
+        borderRadius: 20,
+        margin: EdgeInsets.all(15),
+        colorText: Colors.black,
+        duration: Duration(seconds: 4),
+        isDismissible: true,
+        dismissDirection: DismissDirection.horizontal,
+        forwardAnimationCurve: Curves.easeOutBack,
+      );
+    } else {
+      RealtimeDb.updateBucketEntry(
+          bucketModel.bucketId, data, bucketData['entryId']);
+      Get.snackbar(
+        'Success!',
+        'Updated new entry!',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: kprimaryColor,
+        borderRadius: 20,
+        margin: EdgeInsets.all(15),
+        colorText: Colors.black,
+        duration: Duration(seconds: 4),
+        isDismissible: true,
+        dismissDirection: DismissDirection.horizontal,
+        forwardAnimationCurve: Curves.easeOutBack,
+      );
+    }
   }
 }
