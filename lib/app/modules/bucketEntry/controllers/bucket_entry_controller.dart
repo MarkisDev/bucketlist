6b3e9a5100e0d9cc5f15d8f318f78407b8a33d92
Rijuth Menon <rijuthm@gmail.com>
2022-08-07 11:37:01 +0530

    storing bucketEntry info

--- lib/app/modules/bucketEntry/controllers/bucket_entry_controller.dart
+++ lib/app/modules/bucketEntry/controllers/bucket_entry_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:bucketlist/app/data/providers/realtime_provider.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:get/get.dart';
 
@@ -7,6 +8,8 @@ class BucketEntryController extends GetxController {
   QuillController quillController = QuillController.basic();
   final titleName = "".obs;
   final count = 0.obs;
+  final bucketModel = Get.arguments['bucketModel'];
+  final userModel = Get.arguments['userModel'];
   @override
   void onInit() {
     super.onInit();
@@ -20,7 +23,17 @@ class BucketEntryController extends GetxController {
   @override
   void onClose() {
     var json = jsonEncode(quillController.document.toDelta().toJson());
-    print(json);
-    print(titleName);
+    var data = {
+      "title": titleName.value,
+      "json": json,
+      "creatorInfo": {
+        "id": userModel.id,
+        "fullName": userModel.fullName,
+        "firstName": userModel.firstName,
+        "lastName": userModel.lastName,
+        "email": userModel.email,
+      }
+    };
+    RealtimeDb.addBucketEntry(bucketModel.bucketId, data);
   }
 }
