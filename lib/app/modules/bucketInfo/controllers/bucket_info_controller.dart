689a0d6248f47b1d4d1ae364e04bb483990f0c1c
Rijuth Menon <rijuthm@gmail.com>
2022-08-07 11:37:27 +0530

    changed args to store userModel for bucketEntry

--- lib/app/modules/bucketInfo/controllers/bucket_info_controller.dart
+++ lib/app/modules/bucketInfo/controllers/bucket_info_controller.dart
@@ -1,16 +1,34 @@
+import 'package:bucketlist/app/data/models/user_model.dart';
+import 'package:bucketlist/app/data/providers/realtime_provider.dart';
+import 'package:bucketlist/app/data/repositories/user_repository.dart';
+import 'package:bucketlist/app/modules/home/controllers/home_controller.dart';
 import 'package:get/get.dart';
 
 class BucketInfoController extends GetxController {
-  var args = Get.arguments;
+  var bucketModel = Get.arguments;
+  final UserRepository repository;
+  late final UserModel userModel;
+  BucketInfoController({required this.repository});
+  var newEntry = false;
+  var entries = [].obs;
 
   @override
   void onInit() {
     super.onInit();
+    HomeController homeController = Get.find<HomeController>();
+    userModel = homeController.args;
+    // var x = repository.getBucketEntries(bucketModel.bucketId).listen((event) {
+    //   entries.value = event;
+    // });
+    entries.bindStream(repository.getBucketEntries(bucketModel.bucketId));
   }
 
   @override
   void onReady() {
     super.onReady();
+    // entries.listen((p0) {
+    //   print("READY : ${p0}");
+    // });
   }
 
   @override
