26aa0399ee3f2c7417efc7ba8103851b0ba009c8
Rijuth Menon <rijuthm@gmail.com>
2022-09-11 21:36:43 +0530

    lazyPut login controller for logout and unprecendented bucket deletion

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,4 +1,5 @@
 import 'package:bucketlist/app/data/repositories/user_repository.dart';
+import 'package:bucketlist/app/modules/login/controllers/login_controller.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
@@ -8,6 +9,9 @@ class HomeBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<HomeController>(
       () => HomeController(repository: UserRepository()),
+    );
+    Get.lazyPut<LoginController>(
+      () => LoginController(repository: UserRepository()),
     );
   }
 }
