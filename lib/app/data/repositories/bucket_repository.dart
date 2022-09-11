f8fc9bcf52df88b06ea420c1211b0c0e04229677
Rijuth Menon <rijuthm@gmail.com>
2022-09-11 12:25:56 +0530

    created bucket repository to hold bucketEntry functions

---
+++ lib/app/data/repositories/bucket_repository.dart
@@ -0,0 +1,33 @@
+import 'package:bucketlist/app/data/providers/realtime_provider.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class BucketRepository {
+  deleteBucketEntry(String bucketId, String entryId) {
+    RealtimeDb.deleteBucketEntry(bucketId, entryId);
+  }
+
+  getBucketEntries(String bucketId) {
+    var bucketSnapshot = RealtimeDb.getBucketInfo(bucketId);
+    return bucketSnapshot.map((DatabaseEvent event) {
+      List entries = [];
+
+      Map bucket = Map.of(event.snapshot.value as Map);
+      if (bucket.containsKey('entries') && bucket['entries'].length > 0) {
+        for (final key in bucket['entries'].keys) {
+          Map data = bucket['entries'][key];
+          data['entryId'] = key;
+          entries.add(bucket['entries'][key]);
+        }
+      }
+      return entries;
+    });
+  }
+
+  addBucketEntry(String bucketId, Map data, String userId) {
+    RealtimeDb.addBucketEntry(bucketId, data, userId);
+  }
+
+  updateBucketEntry(String bucketId, Map data, String entryId) {
+    RealtimeDb.updateBucketEntry(bucketId, data, entryId);
+  }
+}
