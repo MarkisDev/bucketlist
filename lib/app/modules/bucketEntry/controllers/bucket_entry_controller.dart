c0097360cfe1e64915e571f8b74211524174a982
Rijuth Menon <rijuthm@gmail.com>
2022-09-11 15:35:03 +0530

    added check for empty title

--- lib/app/modules/bucketEntry/controllers/bucket_entry_controller.dart
+++ lib/app/modules/bucketEntry/controllers/bucket_entry_controller.dart
@@ -5,13 +5,14 @@ import 'package:bucketlist/app/data/repositories/bucket_repository.dart';
 import 'package:bucketlist/app/modules/bucketInfo/controllers/bucket_info_controller.dart';
 import 'package:bucketlist/app/ui/theme/color_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
 import 'package:get/get.dart';
 
 class BucketEntryController extends GetxController {
   BucketEntryController({required this.repository});
   final BucketRepository repository;
-  late QuillController quillController;
+  late quill.QuillController quillController;
+  var bucketController = Get.find<BucketInfoController>();
   final titleController = TextEditingController();
   final count = 0.obs;
   final bucketModel = Get.arguments['bucketModel'];
@@ -19,17 +20,17 @@ class BucketEntryController extends GetxController {
   late final bucketData;
   @override
   void onInit() {
-    super.onInit();
+    // super.onInit();
     var bucketController = Get.find<BucketInfoController>();
     if (!bucketController.newEntry) {
       bucketData = Get.arguments['bucketData'];
-      var document = jsonDecode(bucketData['json']);
-      titleController.text = bucketData['title'];
-      quillController = QuillController(
-          document: Document.fromJson(document),
+      var document = jsonDecode(bucketData['entryInfo']['json']);
+      titleController.text = bucketData['entryInfo']['title'];
+      quillController = quill.QuillController(
+          document: quill.Document.fromJson(document),
           selection: TextSelection.collapsed(offset: 0));
     } else {
-      quillController = QuillController.basic();
+      quillController = quill.QuillController.basic();
     }
   }
 
@@ -39,75 +40,146 @@ class BucketEntryController extends GetxController {
   }
 
   @override
-  void onClose() {}
+  void onClose() {
+    if (!bucketController.newEntry) {
+      var data = {
+        "lock": false,
+        "id": userModel.id,
+        "fullName": userModel.fullName,
+        "firstName": userModel.firstName,
+        "lastName": userModel.lastName,
+        "email": userModel.email,
+      };
+      repository.updateBucketMutex(
+          bucketModel.bucketId, data, bucketData['entryId']);
+    }
+  }
 
   addOrUpdateBucketEntry() {
-    var bucketController = Get.find<BucketInfoController>();
-
     var json = jsonEncode(quillController.document.toDelta().toJson());
-
-    if (bucketController.newEntry) {
-      var data = {
-        "title": titleController.text,
-        "json": json,
-        "creatorInfo": {
-          "id": userModel.id,
-          "fullName": userModel.fullName,
-          "firstName": userModel.firstName,
-          "lastName": userModel.lastName,
-          "email": userModel.email,
-        },
-        "mutexInfo": {
-          "lock": false,
-          "id": userModel.id,
-          "fullName": userModel.fullName,
-          "firstName": userModel.firstName,
-          "lastName": userModel.lastName,
-          "email": userModel.email,
-        },
-      };
-      RealtimeDb.addBucketEntry(bucketModel.bucketId, data, userModel.id);
-      Get.snackbar(
-        'Success!',
-        'Added new entry!',
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: kprimaryColor,
-        borderRadius: 20,
-        margin: EdgeInsets.all(15),
-        colorText: Colors.black,
-        duration: Duration(seconds: 4),
-        isDismissible: true,
-        dismissDirection: DismissDirection.horizontal,
-        forwardAnimationCurve: Curves.easeOutBack,
-      );
+    if (titleController.text.isEmpty) {
+      Get.defaultDialog(
+          title: "Error!",
+          titlePadding: EdgeInsets.fromLTRB(0, 21, 0, 0),
+          backgroundColor: kprimaryBackgroundColor,
+          titleStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 23,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500),
+          contentPadding: EdgeInsets.all(21),
+          content: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Please enter a title to save!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w400),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          "Okay",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontFamily: 'Raleway'),
+                        ),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              ksecondaryBackgroundColor),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(18.0),
+                                      side: BorderSide(color: Colors.black)))),
+                      onPressed: () async {
+                        Get.back();
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ));
     } else {
-      var data = {
-        "title": titleController.text,
-        "json": json,
-        "mutexInfo": {
-          "lock": false,
-          "id": userModel.id,
-          "fullName": userModel.fullName,
-          "firstName": userModel.firstName,
-          "lastName": userModel.lastName,
-          "email": userModel.email,
-        },
-      };
-      RealtimeDb.updateBucketEntry(
-          bucketModel.bucketId, data, bucketData['entryId']);
-      Get.snackbar(
-        'Success!',
-        'Updated new entry!',
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: kprimaryColor,
-        borderRadius: 20,
-        margin: EdgeInsets.all(15),
-        colorText: Colors.black,
-        duration: Duration(seconds: 4),
-        isDismissible: true,
-        dismissDirection: DismissDirection.horizontal,
-        forwardAnimationCurve: Curves.easeOutBack,
-      );
+      if (bucketController.newEntry) {
+        var data = {
+          "entryInfo": {
+            "title": titleController.text,
+            "json": json,
+          },
+          "creatorInfo": {
+            "id": userModel.id,
+            "fullName": userModel.fullName,
+            "firstName": userModel.firstName,
+            "lastName": userModel.lastName,
+            "email": userModel.email,
+          },
+          "mutexInfo": {
+            "lock": false,
+            "id": userModel.id,
+            "fullName": userModel.fullName,
+            "firstName": userModel.firstName,
+            "lastName": userModel.lastName,
+            "email": userModel.email,
+          },
+        };
+        RealtimeDb.addBucketEntry(bucketModel.bucketId, data, userModel.id);
+        Get.snackbar(
+          'Success!',
+          'Added new entry!',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: kprimaryColor,
+          borderRadius: 20,
+          margin: EdgeInsets.all(15),
+          colorText: Colors.black,
+          duration: Duration(seconds: 4),
+          isDismissible: true,
+          dismissDirection: DismissDirection.horizontal,
+          forwardAnimationCurve: Curves.easeOutBack,
+        );
+      } else {
+        var data = {
+          "entryInfo": {
+            "title": titleController.text,
+            "json": json,
+          },
+          "mutexInfo": {
+            "lock": false,
+            "id": userModel.id,
+            "fullName": userModel.fullName,
+            "firstName": userModel.firstName,
+            "lastName": userModel.lastName,
+            "email": userModel.email,
+          },
+        };
+        RealtimeDb.updateBucketEntry(
+            bucketModel.bucketId, data, bucketData['entryId']);
+        Get.snackbar(
+          'Success!',
+          'Updated new entry!',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: kprimaryColor,
+          borderRadius: 20,
+          margin: EdgeInsets.all(15),
+          colorText: Colors.black,
+          duration: Duration(seconds: 4),
+          isDismissible: true,
+          dismissDirection: DismissDirection.horizontal,
+          forwardAnimationCurve: Curves.easeOutBack,
+        );
+      }
     }
   }
 }
