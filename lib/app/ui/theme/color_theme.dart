e10364e9aa1e2ae94cf38fb43153ba223674c6fd
Rijuth Menon <rijuthm@gmail.com>
2023-04-01 19:08:18 +0530

    added const for performance in constant color values

--- lib/app/ui/theme/color_theme.dart
+++ lib/app/ui/theme/color_theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-Color kprimaryColor = Color(0xffAFFC41);
-Color ksecondaryColor = Color(0xffB8E9C4);
+Color kprimaryColor = const Color(0xffAFFC41);
+Color ksecondaryColor = const Color(0xffB8E9C4);
 Color kprimaryBackgroundColor = Colors.black;
-Color ksecondaryBackgroundColor = Color(0xff1d1d1d);
+Color ksecondaryBackgroundColor = const Color(0xff1d1d1d);
