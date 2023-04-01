e36550fbe2b1cc46a3b484cce4feb3184d6f25fa
Rijuth Menon <rijuthm@gmail.com>
2023-04-01 19:05:34 +0530

    restructured entry controller for firestore

--- lib/app/modules/bucketEntry/controllers/bucket_entry_controller.dart
+++ lib/app/modules/bucketEntry/controllers/bucket_entry_controller.dart
@@ -1,147 +1,44 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:bucketlist/app/data/providers/realtime_provider.dart';
-import 'package:bucketlist/app/data/repositories/bucket_repository.dart';
+import 'package:bucketlist/app/data/models/bucket_entries_model.dart';
+import 'package:bucketlist/app/data/models/bucket_model.dart';
+import 'package:bucketlist/app/data/models/user_model.dart';
+import 'package:bucketlist/app/data/providers/firestore_provider.dart';
 import 'package:bucketlist/app/modules/bucketInfo/controllers/bucket_info_controller.dart';
 import 'package:bucketlist/app/ui/theme/color_theme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart' as quill;
 import 'package:get/get.dart';
 
 class BucketEntryController extends GetxController {
-  BucketEntryController({required this.repository});
-  final BucketRepository repository;
   late quill.QuillController quillController;
   var bucketController = Get.find<BucketInfoController>();
   final titleController = TextEditingController();
-  final count = 0.obs;
-  final bucketModel = Get.arguments['bucketModel'];
-  final userModel = Get.arguments['userModel'];
-  late final bucketData;
-  var criticalInfoChanged = false.obs;
-  late StreamSubscription criticalInfoSub;
-  @override
-  void onInit() {
-    super.onInit();
-    // Adding a stream binding to keep track of bucket being deleted
-    criticalInfoChanged
-        .bindStream(repository.criticalBucketInfoChanged(bucketModel.bucketId));
-    // Listening to changes and deleting
-    criticalInfoSub = criticalInfoChanged.listen((p0) {
-      if (p0) {
-        Get.defaultDialog(
-            title: "Error!",
-            titlePadding: EdgeInsets.fromLTRB(0, 21, 0, 0),
-            backgroundColor: kprimaryColor,
-            titleStyle: TextStyle(
-                color: Colors.black,
-                fontSize: 23,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w500),
-            contentPadding: EdgeInsets.all(21),
-            content: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "This bucket has been deleted!",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w400),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 12.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      TextButton(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                          child: Text(
-                            "Go back!",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontFamily: 'Raleway'),
-                          ),
-                        ),
-                        style: ButtonStyle(
-                            overlayColor: MaterialStateProperty.all(
-                                Colors.white.withOpacity(0.15)),
-                            backgroundColor: MaterialStateProperty.all(
-                                ksecondaryBackgroundColor),
-                            shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.black)))),
-                        onPressed: () async {
-                          Get.toNamed('/home', arguments: userModel);
-                        },
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ));
-      }
-    });
-    var bucketController = Get.find<BucketInfoController>();
-    if (!bucketController.newEntry) {
-      bucketData = Get.arguments['bucketData'];
-      var document = jsonDecode(bucketData['entryInfo']['json']);
-      titleController.text = bucketData['entryInfo']['title'];
-      quillController = quill.QuillController(
-          document: quill.Document.fromJson(document),
-          selection: TextSelection.collapsed(offset: 0));
-    } else {
-      quillController = quill.QuillController.basic();
-    }
-  }
+  BucketModel bucketModel = Get.arguments['bucketModel'];
+  final UserModel userModel = Get.arguments['userModel'];
+  late final BucketEntriesModel bucketData;
+  late Stream<DocumentSnapshot> bucketStream;
+  late StreamSubscription bucketStreamSub;
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() async {
-    await criticalInfoSub.cancel();
-
-    if (!bucketController.newEntry) {
-      var mutex = {
-        "lock": false,
-        "id": userModel.id,
-        "fullName": userModel.fullName,
-        "firstName": userModel.firstName,
-        "lastName": userModel.lastName,
-        "email": userModel.email,
-        "timestamp": DateTime.now().toUtc().toIso8601String(),
-      };
-      repository.updateBucketMutex(
-          bucketModel.bucketId, mutex, bucketData['entryId']);
-    }
-  }
-
-  addOrUpdateBucketEntry() {
+  addOrUpdateBucketEntry() async {
     var json = jsonEncode(quillController.document.toDelta().toJson());
     if (titleController.text.isEmpty) {
       Get.defaultDialog(
           title: "Error!",
-          titlePadding: EdgeInsets.fromLTRB(0, 21, 0, 0),
+          titlePadding: const EdgeInsets.fromLTRB(0, 21, 0, 0),
           backgroundColor: kprimaryBackgroundColor,
-          titleStyle: TextStyle(
+          titleStyle: const TextStyle(
               color: Colors.white,
               fontSize: 23,
               fontFamily: 'Poppins',
               fontWeight: FontWeight.w500),
-          contentPadding: EdgeInsets.all(21),
+          contentPadding: const EdgeInsets.all(21),
           content: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
+              const Text(
                 "Please enter a title to save!",
                 style: TextStyle(
                     color: Colors.white,
@@ -155,16 +52,6 @@ class BucketEntryController extends GetxController {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     TextButton(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Text(
-                          "Okay",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontFamily: 'Raleway'),
-                        ),
-                      ),
                       style: ButtonStyle(
                           overlayColor: MaterialStateProperty.all(
                               Colors.white.withOpacity(0.15)),
@@ -174,10 +61,21 @@ class BucketEntryController extends GetxController {
                               MaterialStateProperty.all<RoundedRectangleBorder>(
                                   RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(18.0),
-                                      side: BorderSide(color: Colors.black)))),
+                                      side: const BorderSide(
+                                          color: Colors.black)))),
                       onPressed: () async {
                         Get.back();
                       },
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          "Okay",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontFamily: 'Raleway'),
+                        ),
+                      ),
                     ),
                   ],
                 ),
@@ -185,76 +83,175 @@ class BucketEntryController extends GetxController {
             ],
           ));
     } else {
-      if (bucketController.newEntry) {
-        var data = {
-          "entryInfo": {
-            "title": titleController.text,
-            "json": json,
-          },
-          "creatorInfo": {
-            "id": userModel.id,
-            "fullName": userModel.fullName,
-            "firstName": userModel.firstName,
-            "lastName": userModel.lastName,
-            "email": userModel.email,
-            "timestamp": DateTime.now().toUtc().toIso8601String(),
-          },
-          "mutexInfo": {
-            "lock": false,
-            "id": userModel.id,
-            "fullName": userModel.fullName,
-            "firstName": userModel.firstName,
-            "lastName": userModel.lastName,
-            "email": userModel.email,
-            "timestamp": DateTime.now().toUtc().toIso8601String(),
-          },
-        };
-        RealtimeDb.addBucketEntry(bucketModel.bucketId, data, userModel.id);
-        Get.snackbar(
-          'Success!',
-          'Added new entry!',
-          snackPosition: SnackPosition.BOTTOM,
-          backgroundColor: kprimaryColor,
-          borderRadius: 20,
-          margin: EdgeInsets.all(15),
-          colorText: Colors.black,
-          duration: Duration(seconds: 4),
-          isDismissible: true,
-          dismissDirection: DismissDirection.horizontal,
-          forwardAnimationCurve: Curves.easeOutBack,
-        );
-      } else {
-        var data = {
-          "entryInfo": {
-            "title": titleController.text,
-            "json": json,
-          },
-          "mutexInfo": {
-            "lock": false,
-            "id": userModel.id,
-            "fullName": userModel.fullName,
-            "firstName": userModel.firstName,
-            "lastName": userModel.lastName,
-            "email": userModel.email,
-            "timestamp": DateTime.now().toUtc().toIso8601String(),
-          },
-        };
-        RealtimeDb.updateBucketEntry(
-            bucketModel.bucketId, data, bucketData['entryId']);
+      var data = {
+        "entryInfo": {
+          "title": titleController.text,
+          "json": json,
+        },
+        "mutexInfo": {
+          "lock": true,
+          "id": userModel.id,
+          "fullName": userModel.fullName,
+          "firstName": userModel.firstName,
+          "lastName": userModel.lastName,
+          "email": userModel.email,
+          "timestamp": DateTime.now().toUtc().toIso8601String(),
+        },
+      };
+      if (bucketController.newEntry == false) {
         Get.snackbar(
           'Success!',
           'Updated new entry!',
           snackPosition: SnackPosition.BOTTOM,
           backgroundColor: kprimaryColor,
           borderRadius: 20,
-          margin: EdgeInsets.all(15),
+          margin: const EdgeInsets.all(15),
           colorText: Colors.black,
-          duration: Duration(seconds: 4),
+          duration: const Duration(seconds: 4),
           isDismissible: true,
           dismissDirection: DismissDirection.horizontal,
           forwardAnimationCurve: Curves.easeOutBack,
         );
+        // Updating exisitng bucket
+        await FirestoreDb.updateBucketEntry(
+            bucketModel.id!, data, bucketData.id!);
+      } else {
+        // Adding creator info for new entry
+        data['creatorInfo'] = {
+          "id": userModel.id,
+          "fullName": userModel.fullName,
+          "firstName": userModel.firstName,
+          "lastName": userModel.lastName,
+          "email": userModel.email,
+          "timestamp": DateTime.now().toUtc().toIso8601String(),
+        };
+
+        Get.snackbar(
+          'Success!',
+          'Added new entry!',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: kprimaryColor,
+          borderRadius: 20,
+          margin: const EdgeInsets.all(15),
+          colorText: Colors.black,
+          duration: const Duration(seconds: 4),
+          isDismissible: true,
+          dismissDirection: DismissDirection.horizontal,
+          forwardAnimationCurve: Curves.easeOutBack,
+        );
+
+        String entryDocId = await FirestoreDb.addBucketEntry(
+            userModel.id, bucketModel.id!, data);
+        bucketData =
+            await FirestoreDb.getBucketEntry(bucketModel.id!, entryDocId);
       }
     }
+  }
+
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+
+    var bucketController = Get.find<BucketInfoController>();
+    if (!bucketController.newEntry) {
+      bucketData = Get.arguments['bucketData'];
+      var document = jsonDecode(bucketData.entryInfo['json']);
+      titleController.text = bucketData.entryInfo['title'];
+      quillController = quill.QuillController(
+          document: quill.Document.fromJson(document),
+          selection: const TextSelection.collapsed(offset: 0));
+    } else {
+      quillController = quill.QuillController.basic();
+    }
+
+    bucketStream = await FirestoreDb.getBucketStream(bucketModel.id!);
+    // Listening to bucket to ensure that bucket isn't deleted when it is open
+    // Ideally won't happen on one phone, but if logged in same account using two phones this can arise
+    bucketStreamSub = bucketStream.listen((DocumentSnapshot docSnap) {
+      if (!docSnap.exists) {
+        Get.defaultDialog(
+            title: "Error!",
+            titlePadding: const EdgeInsets.fromLTRB(0, 21, 0, 0),
+            backgroundColor: kprimaryColor,
+            titleStyle: const TextStyle(
+                color: Colors.black,
+                fontSize: 23,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w500),
+            contentPadding: const EdgeInsets.all(21),
+            content: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "This bucket has been deleted!",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      TextButton(
+                        style: ButtonStyle(
+                            overlayColor: MaterialStateProperty.all(
+                                Colors.white.withOpacity(0.15)),
+                            backgroundColor: MaterialStateProperty.all(
+                                ksecondaryBackgroundColor),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: const BorderSide(
+                                        color: Colors.black)))),
+                        onPressed: () async {
+                          Get.toNamed('/home', arguments: userModel);
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Text(
+                            "Go back!",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontFamily: 'Raleway'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ));
+      } else {
+        // Always storing latest copy of bucket
+        bucketModel =
+            BucketModel.fromDocumentSnapshot(documentSnapshot: docSnap);
+      }
+    });
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() async {
+    await bucketStreamSub.cancel();
+
+    var mutex = {
+      "lock": false,
+      "id": userModel.id,
+      "fullName": userModel.fullName,
+      "firstName": userModel.firstName,
+      "lastName": userModel.lastName,
+      "email": userModel.email,
+      "timestamp": DateTime.now().toUtc().toIso8601String(),
+    };
+    await FirestoreDb.updateMutex(bucketModel.id!, bucketData.id!, mutex);
   }
 }
